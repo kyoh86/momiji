3cd2d09a8d7fbb9779ce61a9c3392a19591161b7
kyoh86 <me@kyoh86.dev>
2024-11-24 18:03:37 +0900

    rename light to bright

--- color.go
+++ color.go
@@ -10,11 +10,21 @@ type Color struct {
 	Index uint8
 }
 
-func (c Color) HexRGB() string {
+// ToHex forms rrggbb
+func (c Color) ToHex() string {
 	return fmt.Sprintf("%02x%02x%02x", c.RGBA.R, c.RGBA.G, c.RGBA.B)
 }
 
-// DecRGBFunc forms rgb(255, 255, 255)
-func (c Color) DecRGBFunc() string {
+// ToRGBFunc forms rgb(255, 255, 255)
+func (c Color) ToRGBFunc() string {
 	return fmt.Sprintf("rgb(%d,%d,%d)", c.RGBA.R, c.RGBA.G, c.RGBA.B)
+}
+
+// FromHex converts rrggbb to color.RGBA
+func FromHex(hex string) (v color.RGBA) {
+	_, err := fmt.Sscanf(hex, "%02x%02x%02x", &v.R, &v.G, &v.B)
+	if err != nil {
+		panic(err)
+	}
+	return
 }
--- palette.go
+++ palette.go
@@ -1,145 +1,117 @@
 package main
 
-import (
-	"image/color"
-)
-
-type Name string
-
 type ColorPalette struct {
-	Black        Color
-	Red          Color
-	Green        Color
-	Yellow       Color
-	Blue         Color
-	Magenta      Color
-	Cyan         Color
-	White        Color
-	LightBlack   Color
-	LightRed     Color
-	LightGreen   Color
-	LightYellow  Color
-	LightBlue    Color
-	LightMagenta Color
-	LightCyan    Color
-	LightWhite   Color
-	HardBlack    Color
-	GrayScale1   Color
-	GrayScale2   Color
-	GrayScale3   Color
-	GrayScale4   Color
-	GrayScale5   Color
+	Black         Color
+	Red           Color
+	Green         Color
+	Yellow        Color
+	Blue          Color
+	Magenta       Color
+	Cyan          Color
+	White         Color
+	BrightBlack   Color
+	BrightRed     Color
+	BrightGreen   Color
+	BrightYellow  Color
+	BrightBlue    Color
+	BrightMagenta Color
+	BrightCyan    Color
+	BrightWhite   Color
+	HardBlack     Color
+	GrayScale1    Color
+	GrayScale2    Color
+	GrayScale3    Color
+	GrayScale4    Color
+	GrayScale5    Color
 }
 
 var Palette = ColorPalette{
 	Black: Color{
-		// rgb( 20,  12,  12) rgb(  7.8%,   4.7%,   4.7%)
-		RGBA:  color.RGBA{R: 0x14, G: 0x0c, B: 0x0c},
+		RGBA:  FromHex("140c0c"),
 		Index: 0,
 	},
 	Red: Color{
-		// rgb(218,  87, 116) rgb( 85.5%,  34.1%,  45.5%)
-		RGBA:  color.RGBA{R: 0xda, G: 0x57, B: 0x74},
+		RGBA:  FromHex("da5774"),
 		Index: 1,
 	},
 	Green: Color{
-		// rgb( 52, 140,  78) rgb( 20.4%,  54.9%,  30.6%)
-		RGBA:  color.RGBA{R: 0x34, G: 0x8c, B: 0x4e},
+		RGBA:  FromHex("348c4e"),
 		Index: 2,
 	},
 	Yellow: Color{
-		// rgb(231, 168,  43) rgb( 90.6%,  65.9%,  16.9%)
-		RGBA:  color.RGBA{R: 0xe7, G: 0xa8, B: 0x2b},
+		RGBA:  FromHex("e7a82b"),
 		Index: 3,
 	},
 	Blue: Color{
-		// rgb( 72, 132, 210) rgb( 28.2%,  51.8%,  82.4%)
-		RGBA:  color.RGBA{R: 0x48, G: 0x84, B: 0xd2},
+		RGBA:  FromHex("4884d2"),
 		Index: 4,
 	},
 	Magenta: Color{
-		// rgb(165,  90, 170) rgb( 64.7%,  35.3%,  66.7%)
-		RGBA:  color.RGBA{R: 0xa5, G: 0x5a, B: 0xaa},
+		RGBA:  FromHex("a55aaa"),
 		Index: 5,
 	},
 	Cyan: Color{
-		// rgb( 31, 144, 168) rgb( 12.2%,  56.5%,  65.9%)
-		RGBA:  color.RGBA{R: 0x1f, G: 0x90, B: 0xa8},
+		RGBA:  FromHex("1f90a8"),
 		Index: 6,
 	},
 	White: Color{
-		// rgb(160, 153, 153) rgb( 62.7%,  60.0%,  60.0%)
-		RGBA:  color.RGBA{R: 0xa0, G: 0x99, B: 0x99},
+		RGBA:  FromHex("a09999"),
 		Index: 7,
 	},
-	LightBlack: Color{
-		// rgb( 90,  78,  78) rgb( 35.3%,  30.6%,  30.6%)
-		RGBA:  color.RGBA{R: 0x5a, G: 0x4e, B: 0x4e},
+	BrightBlack: Color{
+		RGBA:  FromHex("5a4e4e"),
 		Index: 8,
 	},
-	LightRed: Color{
-		// rgb(245, 135,  91) rgb( 96.1%,  52.9%,  35.7%)
-		RGBA:  color.RGBA{R: 0xf5, G: 0x87, B: 0x5b},
+	BrightRed: Color{
+		RGBA:  FromHex("f5875b"),
 		Index: 9,
 	},
-	LightGreen: Color{
-		// rgb(154, 190, 134) rgb( 60.4%,  74.5%,  52.5%)
-		RGBA:  color.RGBA{R: 0x9a, G: 0xbe, B: 0x86},
+	BrightGreen: Color{
+		RGBA:  FromHex("9abe86"),
 		Index: 10,
 	},
-	LightYellow: Color{
-		// rgb(255, 215, 145) rgb(100.0%,  84.3%,  56.9%)
-		RGBA:  color.RGBA{R: 0xff, G: 0xd7, B: 0x91},
+	BrightYellow: Color{
+		RGBA:  FromHex("ffd791"),
 		Index: 11,
 	},
-	LightBlue: Color{
-		// rgb(137, 183, 225) rgb( 53.7%,  71.8%,  88.2%)
-		RGBA:  color.RGBA{R: 0x89, G: 0xb7, B: 0xe1},
+	BrightBlue: Color{
+		RGBA:  FromHex("89b7e1"),
 		Index: 12,
 	},
-	LightMagenta: Color{
-		// rgb(238, 161, 209) rgb( 93.3%,  63.1%,  82.0%)
-		RGBA:  color.RGBA{R: 0xee, G: 0xa1, B: 0xd1},
+	BrightMagenta: Color{
+		RGBA:  FromHex("eea1d1"),
 		Index: 13,
 	},
-	LightCyan: Color{
-		// rgb(105, 178, 172) rgb( 41.2%,  69.8%,  67.5%)
-		RGBA:  color.RGBA{R: 0x69, G: 0xb2, B: 0xac},
+	BrightCyan: Color{
+		RGBA:  FromHex("69b2ac"),
 		Index: 14,
 	},
-	LightWhite: Color{
-		// rgb(230, 227, 227) rgb( 90.2%,  89.0%,  89.0%)
-		RGBA:  color.RGBA{R: 0xe6, G: 0xe3, B: 0xe3},
+	BrightWhite: Color{
+		RGBA:  FromHex("e6e3e3"),
 		Index: 15,
 	},
 	HardBlack: Color{
-		// rgb(  8,   8,   8) rgb(  3.1%,   3.1%,   3.1%)
-		RGBA:  color.RGBA{R: 0x08, G: 0x08, B: 0x08},
+		RGBA:  FromHex("080808"),
 		Index: 232,
 	},
 	GrayScale1: Color{
-		// rgb( 55,  42,  42) rgb( 21.6%,  16.5%,  16.5%)
-		RGBA:  color.RGBA{R: 0x37, G: 0x2a, B: 0x2a},
+		RGBA:  FromHex("372a2a"),
 		Index: 235,
 	},
 	GrayScale2: Color{
-		// rgb( 90,  78,  78) rgb( 35.3%,  30.6%,  30.6%)
-		RGBA:  color.RGBA{R: 0x5a, G: 0x4e, B: 0x4e},
+		RGBA:  FromHex("5a4e4e"),
 		Index: 236,
 	},
 	GrayScale3: Color{
-		// rgb(125, 115, 115) rgb( 49.0%,  45.1%,  45.1%)
-		RGBA:  color.RGBA{R: 0x7d, G: 0x73, B: 0x73},
+		RGBA:  FromHex("7d7373"),
 		Index: 237,
 	},
 	GrayScale4: Color{
-		// rgb(160, 153, 153) rgb( 62.7%,  60.0%,  60.0%)
-		RGBA:  color.RGBA{R: 0xa0, G: 0x99, B: 0x99},
+		RGBA:  FromHex("a09999"),
 		Index: 238,
 	},
 	GrayScale5: Color{
-		// rgb(195, 190, 190) rgb( 76.5%,  74.5%,  74.5%)
-		RGBA:  color.RGBA{R: 0xc3, G: 0xbe, B: 0xbe},
+		RGBA:  FromHex("c3bebe"),
 		Index: 239,
 	},
 }
